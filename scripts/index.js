cd8da01beaf9f86a19273dd07991f54804a82c94
Shreyas <helloshreyasj@gmail.com>
2025-04-20 13:56:53 +0100

    Made changes to index.js, reset.html, dashboard.html, log-in.html and sign-up.html.
    For all pages, logo now redirects to the index page when clicked.
    Implemented input validation for email in reset.html.
    Implemented logout functionality in index.html.
    Pages will look different on whether logged out or signed in.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ let button_signup = document.getElementById('signup');
 let button_login = document.getElementById('login');
 let button_getstarted = document.getElementById('get-started');
 
+let replaceTo = 'pages/sign-up.html'; //replace location to pages
+
 logo.addEventListener('click,', () => {
     location.replace('index.html');
 });
@@ -13,5 +15,21 @@ button_signup.addEventListener('click', () => {
     location.replace('pages/sign-up.html')
 });
 button_getstarted.addEventListener('click', () => {
-    location.replace('pages/sign-up.html');
+    location.replace(replaceTo);
 })
+
+let success = localStorage.getItem("loginSuccess");
+
+if(success === "true") {
+    button_signup.style.visibility = "hidden";
+    button_login.textContent = "Log Out";
+    button_getstarted.textContent = "Open Dashboard";
+    replaceTo = "pages/dashboard.html"; //replace location to dashboard
+
+    button_login.addEventListener('click',() => {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+        localStorage.removeItem("email");
+        localStorage.loginSuccess = "false";
+    }); 
+}

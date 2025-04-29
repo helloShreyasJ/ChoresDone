f2ccdcf2fe00ea2c58f074b07f0d8905926fdab6
Shreyas <helloshreyasj@gmail.com>
2025-04-29 12:26:53 +0100

    Added dashboard.js, made changes to dashboard.html
    Moved js to an external file

---
+++ scripts/dashboard.js
@@ -0,0 +1,91 @@
+let logo = document.getElementById('logo');
+let choresDone = document.getElementById('numberOfChoresDone');
+let name = document.getElementById('name');
+let message = document.getElementById('message');
+let success = localStorage.getItem("loginSuccess");
+let button_addPeople = document.getElementById('button_addPeople');
+let button_addChores = document.getElementById('button_addChores');
+let people = [];
+let chores = [];
+
+if (success === 'true') {
+    name.textContent = localStorage.getItem("username");
+    message.textContent = "Today, you are you.";
+}
+
+let taskTable = document.getElementById('taskTable');
+
+function buildTable() {
+    taskTable.textContent = "";
+    //Building table head. It will contain the choreTitles.
+    let thead = document.createElement('thead');
+    let headerRow = document.createElement('tr');
+    let emptyCell = document.createElement('th'); //Left corner needs to be empty because the names go there
+    emptyCell.className = "emptyCell";
+    headerRow.appendChild(emptyCell);
+    
+    //Adding choreTitle for rowHeader using a for loop
+    chores.forEach(chore => {
+        let choreTitle = document.createElement('th');
+        choreTitle.textContent = chore;
+        headerRow.appendChild(choreTitle);
+        headerRow.className = 'choreHeadings';
+    })
+    thead.appendChild(headerRow);
+    taskTable.appendChild(thead);
+
+    //Building the table body. It will contain name of the People.
+    let tbody = document.createElement('tbody');
+    tbody.className = 'tbody';
+        people.forEach(person => { 
+            let personRow = document.createElement('tr');
+            let nameCell = document.createElement('td');
+            nameCell.className = 'nameCell';
+            nameCell.textContent = person;
+            personRow.appendChild(nameCell);
+
+            chores.forEach(chore => {
+            let checkboxCell = document.createElement('td');
+            let checkboxContainer = document.createElement('div');
+            checkboxCell.className = 'checkboxCell';
+            checkboxContainer.className = 'checkbox-container';
+
+            for(let i = 0; i < 7; i++) {
+                let checkbox = document.createElement('input');
+                checkbox.className = 'checkbox';
+                checkbox.type = 'checkbox';
+                checkboxContainer.appendChild(checkbox);
+            }
+
+            checkboxCell.appendChild(checkboxContainer);
+            personRow.appendChild(checkboxCell);
+        })
+        tbody.appendChild(personRow);
+    })
+    taskTable.appendChild(tbody);
+
+    taskTable.className = "taskTable";
+}
+
+let addPeople = () => {
+    let personName = prompt("Enter the person's name:");
+    if (personName) {
+        people.push(personName);
+        buildTable();
+    }
+}
+
+let addChores = () => {
+    let choreName = prompt("Enter the task name:");
+    if (choreName) {
+        chores.push(choreName);
+        buildTable();
+    }
+}
+
+button_addPeople.addEventListener('click', addPeople);
+button_addChores.addEventListener('click', addChores);
+
+logo.addEventListener('click', () => {
+    location.replace('../index.html');
+});
